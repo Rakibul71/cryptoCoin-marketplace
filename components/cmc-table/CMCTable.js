739d9bbf06e3e87e8f5225140078ab702536c2e5
Rakibul71 <rakibulislam202019@gmail.com>
2022-05-24 21:48:24 +0600

    cmc table front end

--- components/cmc-table/CMCTable.js
+++ components/cmc-table/CMCTable.js
@@ -1,6 +1,8 @@
 import React, { useState, useContext, useCallback, useEffect } from 'react'
 import btc from '../../assets/btc.png'
 import { CoinMarketContext } from "../../context/context"
+import CMCtableHeader from "./CMCTableHeader"
+import CMCTableRow from "./CMCTableRow"
 
 const CMCTable = () => {
     let { getTopTenCoins } = useContext(CoinMarketContext)
@@ -27,7 +29,38 @@ const CMCTable = () => {
     }, [getTopTenCoins])
     console.log(coinData)
     return (
-        <div>CmcTable</div>
+        <div className='text-white font-bold'>
+            <div className='mx-auto max-w-screen-2xl'>
+                <table className='w-full'>
+                    <CMCtableHeader />
+
+                    {coinData && coinData ? (
+                        coinData.map((coin, index) => {
+                            return (
+                                <CMCTableRow
+                                    key={index}
+                                    starNum={coin.cmc_rank}
+                                    coinName={coin.name}
+                                    coinSymbol={coin.symbol}
+                                    coinIcon={btc}
+                                    showBuy={true}
+                                    hRate={coin.quote.USD.percent_change_24h}
+                                    dRate={coin.quote.USD.percent_change_7d}
+                                    hRateIsIncrement={true}
+                                    price={coin.quote.USD.price}
+                                    marketCapValue={coin.quote.USD.market_cap}
+                                    volumeCryptoValue={coin.quote.USD.volume_24h}
+                                    volumeValue={coin.total_supply}
+                                    circulatingSupply={coin.circulating_supply}
+                                />
+                            )
+                        })
+                    ) : (
+                        <></>
+                    )}
+                </table>
+            </div>
+        </div>
     )
 }
 
